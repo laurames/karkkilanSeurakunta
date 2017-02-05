4daf726182454427525240fa064dd5a638e3c44f
Laura Meskanen-Kundu <lauramk@Lauras-MBP.Home>
2017-02-05 16:05:58 +0200

    all events on page

--- js/parseHtml.js
+++ js/parseHtml.js
@@ -13,6 +13,7 @@ example Query URL format: www.karkkilanseurakunta.fi%2Ftapahtumat%2F-%2Fhaku%2F0
 
 function weeksEvents(){
   todaysDates();
+  var pages;
   //The current dates for today:
   function todaysDates(){
     var today = new Date();
@@ -34,49 +35,37 @@ function weeksEvents(){
     }
     if(dayOfTheWeek === 1){
       dayOffset = daysToOffsetBy(7);
-      startOfWeek = day + '%2F' + month + '%2F' + year;
-      endOfWeek = dayOffset.getDate() + '%2F' + (dayOffset.getMonth()+1) + '%2F' + dayOffset.getFullYear();
+      startOfWeek = '06%2F2%2F2017';
+      endOfWeek = '12%2F2%2F2017';
+      //startOfWeek = day + '%2F' + month + '%2F' + year;
+      //endOfWeek = dayOffset.getDate() + '%2F' + (dayOffset.getMonth()+1) + '%2F' + dayOffset.getFullYear();
     }else if(dayOfTheWeek === 0){
       dayOffset = daysToOffsetBy(-7);
-      startOfWeek = dayOffset.getDate() + '%2F' + (dayOffset.getMonth()+1) + '%2F' + dayOffset.getFullYear();
-      endOfWeek = day + '%2F' + month + '%2F' + year;
+      startOfWeek = '06%2F2%2F2017';
+      endOfWeek = '12%2F2%2F2017';
+      //startOfWeek = dayOffset.getDate() + '%2F' + (dayOffset.getMonth()+1) + '%2F' + dayOffset.getFullYear();
+      //endOfWeek = day + '%2F' + month + '%2F' + year;
     }else{
       var fromMonday = 1-dayOfTheWeek; //negative number
       var fromSunday = 7-dayOfTheWeek; //positive number
       dayOffset = daysToOffsetBy(fromMonday);
       var dayOffsetSunday = daysToOffsetBy(fromSunday);
-      startOfWeek = dayOffset.getDate() + '%2F' + (dayOffset.getMonth()+1) + '%2F' + dayOffset.getFullYear();
-      endOfWeek = dayOffsetSunday.getDate() + '%2F' + (dayOffsetSunday.getMonth()+1) + '%2F' + dayOffsetSunday.getFullYear();
+      startOfWeek = '06%2F2%2F2017';
+      endOfWeek = '12%2F2%2F2017';
+      //startOfWeek = dayOffset.getDate() + '%2F' + (dayOffset.getMonth()+1) + '%2F' + dayOffset.getFullYear();
+      //endOfWeek = dayOffsetSunday.getDate() + '%2F' + (dayOffsetSunday.getMonth()+1) + '%2F' + dayOffsetSunday.getFullYear();
     }
-    console.log('startOfWeek: '+startOfWeek+' endOfWeek: ' + endOfWeek);
-    buildQueryString(startOfWeek, endOfWeek);
+    //console.log('startOfWeek: '+startOfWeek+' endOfWeek: ' + endOfWeek);
+    findPages();
   }
 
-  function buildQueryString(startOfWeek, endOfWeek){
-    //check how many pages:
-    //select * from html where url="www.karkkilanseurakunta.fi/tapahtumat/-/haku/0/6/2/2017/_/12/2/2017/week/2#events" and xpath='//ul[@class="pagination"]/li'
-    var queryPages = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22www.karkkilanseurakunta.fi%2Ftapahtumat%2F-%2Fhaku%2F0%2F" + startOfWeek + "%2F_%2F" + endOfWeek + "%2Fweek%2F2%23events%22%20and%20xpath%3D'%2F%2Ful%5B%40class%3D%22pagination%22%5D%2Fli'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-    var pages = findPages(queryPages);
+  function buildQueryString(startOfWeek, endOfWeek, pages){
     //the query:
-    var requestQuery = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22www.karkkilanseurakunta.fi%2Ftapahtumat%2F-%2Fhaku%2F0%2F" + startOfWeek + "%2F_%2F" + endOfWeek + "%2Fweek%2F" + pages + "%23events%22%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22event-list-wrapper%22%5D%2Fdiv%5Bcontains(%40class%2C%20%22event-item-list%22)%5D'&format=xml&callback=?"
-    console.log(requestQuery);
-    doAjax(requestQuery);
-  }
-
-  function findPages(queryPages){
-    if(queryPages.match('^http')){
-      $.getJSON(queryPages, function(data){
-        var result = data;
-        var numberOfPages = result.query.count;
-        console.log('pages: '+numberOfPages);
-        if(numberOfPages < 2){
-          return numberOfPages-2;
-        }else{ return 1; }
-      });
-    }else {
-      console.log("check the query request");
+    for(i=1; i<=pages; i++){
+      var requestQuery = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22www.karkkilanseurakunta.fi%2Ftapahtumat%2F-%2Fhaku%2F0%2F" + startOfWeek + "%2F_%2F" + endOfWeek + "%2Fweek%2F"+ i +"%23events%22%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22event-list-wrapper%22%5D%2Fdiv%5Bcontains(%40class%2C%20%22event-item-list%22)%5D'&format=xml&callback=?"
+      doAjax(requestQuery);
     }
-  };
+  }
 
   function doAjax(requestQuery){
     var resultData;
@@ -102,7 +91,33 @@ function weeksEvents(){
     }
   };
 
-  var pictures = [];
+  function findPages(){
+    //check how many pages:
+    //select * from html where url="www.karkkilanseurakunta.fi/tapahtumat/-/haku/0/29/1/2017/_/5/2/2017/week/2#events" and xpath='//ul[@class="pagination"]/li'
+    var queryPages = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22www.karkkilanseurakunta.fi%2Ftapahtumat%2F-%2Fhaku%2F0%2F" + startOfWeek + "%2F_%2F" + endOfWeek + "%2Fweek%2F2%23events%22%20and%20xpath%3D'%2F%2Ful%5B%40class%3D%22pagination%22%5D%2Fli'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+
+    if(queryPages.match('^http')){
+      $.ajax({
+          async: false,
+          url: queryPages,
+          success: function(data) {
+            var result = data;
+            var numberOfPages = result.query.count;
+            if(numberOfPages > 0){
+              pages = numberOfPages-2;
+              return pages;
+            }else{
+              pages= 1;
+              return pages;
+            }
+          }
+      });
+      buildQueryString(startOfWeek, endOfWeek, pages);
+    }else {
+      console.log("check the query request");
+    }
+  };
+
   function filterData(data){
     //filter away the sharing links
     data = data.replace(/<div class="share"[^>]*>[\S\s]*?<\/div>/,'');
@@ -121,6 +136,9 @@ function weeksEvents(){
       var eventDescription = $('.description').detach().html();
       var eventLocation = $('.event-location').detach().html();
       function image(eventName){
+        if(eventName.indexOf('Sähly') != -1){
+          eventName = 'Sahly';
+        }
         switch(eventName){
           case 'Perhemessu': return 'kirkko';
           case 'Naistenpiiri': return 'naisten';
@@ -134,27 +152,14 @@ function weeksEvents(){
           case 'Perjantaikahvila': return 'kahvi';
           case 'Jobbis': return 'jobbis';
           case 'Iltamessu': return 'iltamessu';
-          case (eventName.indexOf('Sähly')>-1): return 'sahly'
-          default: return 'logo'
+          case 'Sahly': return 'sahly';
+          case 'Messu': return 'messu';
+          default: return 'logo';
         }
       }
       if(eventName != undefined){
-        $('#events').append('<div class="singleEvent media col-md-3 col-centered">'+ '<div class="media-left media-middle"><img class="media-object img-circle" src="img/' + image(eventName) + '.png" alt=""></div><div class="media-body"><h4 class="media-heading"><span class="eventName">' + eventName + '</span></h4><p class="timeDate"><span class="eventDate">' + eventDate + '</span><span class="eventTime"> Kello: ' + eventTime + '</span></p><p class="eventDescription">Kuvaus: <span>' + eventDescription + '</span></p><p class="eventLocation">Paikka: ' + eventLocation + '</div></div>');
+        $('#events').append('<div class="singleEvent media col-md-3 col-centered">'+ '<div class="media-left media-middle"><img class="media-object img-circle" src="img/' + image(eventName) + '.png" alt=""></div><div class="media-body"><h1 class="media-heading"><span class="eventName">' + eventName + '</span></h1><p class="timeDate"><span class="eventDate">' + eventDate + '</span><span class="eventTime"> Kello: ' + eventTime + '</span></p><p class="eventDescription">Kuvaus: <span>' + eventDescription + '</span></p><p class="eventLocation">Paikka: ' + eventLocation + '</div></div>');
       }
     });
   }
 }
-/*
-function dayOfTheWeek(){
-  var weekday = new Array(7);
-  weekday[0]=  "Sunnuntai";
-  weekday[1] = "Maanantai";
-  weekday[2] = "Tiistai";
-  weekday[3] = "Keskiviikko";
-  weekday[4] = "Torstai";
-  weekday[5] = "Perjantai";
-  weekday[6] = "Lauantai";
-  var n = weekday[today.getDay()];
-  return n;
-}
-*/
